9fa36dfbae3d6e221751fc8e11e4adf6c65d5142
bisoul1984 <fikertetadesse1403@gmail.com>
2024-09-17 00:02:27 +0300

    update create product list

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Navbar, Footer } from './components/layout';
+import { Footer, Navbar } from './components/layout';
 import Home from './pages/Home';
-
-
-import Container from './components/Container';
-import ProductList from './pages/ProductList';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
+import ProductList from './pages/admin/products/ProductList';
+
 
 
  function App() {
@@ -19,9 +17,9 @@ import NotFound from './pages/NotFound';
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin/products" element={<ProductList />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/admin/products" element={<ProductList />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
       <Footer />
     </BrowserRouter>
@@ -34,12 +32,3 @@ root.render(
      <App />
   </React.StrictMode>
 );
-
-//local storage
-localStorage.setItem("myFirstKey", "This is the value")
-localStorage.setItem("userName", "Bisrat Tadesse")
-localStorage.setItem("myFirstInteger", 7)
-
- 
-let myFirstKey = localStorage.getItem("myFirstKey")
-console.log("myFirstKey:", myFirstKey)
--- src/pages/admin/products/ProductList.js
+++ src/pages/admin/products/ProductList.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Link } from "react-router-dom";
-
 export default function ProductList() {
     return (
         <div className="container my-4">
